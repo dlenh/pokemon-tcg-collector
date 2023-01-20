9d255ba22ab8d60b84115921c48e54c2bf0accc6
David Lenh <dlenh425@gmail.com>
2023-01-19 17:27:58 -0800

    Set up server to read main routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const MongoStore = require("connect-mongo");
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
+const mainRoutes = require("./routes/main");
 
 const PORT = 8888;
 
@@ -15,6 +16,9 @@ app.set("view engine", "ejs");
 app.set(express.static("public")); // static files in public folder
 app.use(express.urlencoded({extended: true})); // parse requests from forms
 
+app.use(logger("dev"));
+
+app.use("/", mainRoutes);
 
 // set up server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
