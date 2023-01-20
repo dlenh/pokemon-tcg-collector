4977a4c231494a03f84cab169a5f2035c9a106c2
David Lenh <dlenh425@gmail.com>
2023-01-19 17:28:15 -0800

    Removed code that wasn't being used

--- routes/main.js
+++ routes/main.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../controllers/auth");
+// const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
-const { ensureAuth, ensureGuest } = require("../middleware/auth");
+// const postsController = require("../controllers/posts");
+// const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 
 router.get("/", homeController.getIndex);
